0f4da527e89794c6298938bc8d7e0d34345b3814
Kiki1875b <kevinheo0413@gmail.com>
2024-11-04 12:34:03 +0900

    feat: umc-week5 init

---
+++ src/dtos/user.dto.js
@@ -0,0 +1,27 @@
+export const bodyToUser = (body) => {
+  const birth = new Date(body.birth);
+
+  return {
+    email: body.email,
+    name: body.name,
+    gender: body.gender,
+    birth,
+    address: body.address || "",
+    detailAddress: body.detailAddress || "",
+    phoneNumber: body.phoneNumber,
+    preferences: body.preferences,
+  };
+};
+
+export const responseFromUser = async (data) => {
+  const {user, preferences} = data;
+  console.log("DATA : ", data);
+  return{
+    email: user[0].email,
+    name: user[0].name,
+    preferCategory: preferences.map((pref) => ({
+      foodCategory: pref.food_category_id,
+      categoryName: pref.name,
+    })),
+  };
+};

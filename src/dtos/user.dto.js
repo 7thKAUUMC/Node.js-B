b74db9d1caa270e819e9002fa7fc24793f6ac94d
jih023 <jih023@naver.com>
2024-11-12 02:12:54 +0900

    5주차 워크북 - 회원가입

---
+++ src/dtos/user.dto.js
@@ -0,0 +1,37 @@
+export const bodyToUser = (body) => {
+    const birth = new Date(body.birth);
+  
+    return {
+      email: body.email,
+      name: body.name,
+      gender: body.gender,
+      birth,
+      address: body.address || "",
+      detailAddress: body.detailAddress || "",
+      phoneNumber: body.phoneNumber,
+      preferences: body.preferences,
+    };
+};
+
+export class UserDTO {
+    constructor({ user, preferences }) {
+      this.email = user.email;
+      this.name = user.name;
+      this.preferCategory = preferences.map(pref => pref.name); // ?? ?? ???? ??
+    }
+  }
+
+// responseFromUser
+export const responseFromUser = (user) => {
+    if (!user) {
+        return null; // ??? ??? ?? ?? null ??
+    }
+    
+    // DTO? ???? ??? ???? ??
+    const userDTO = new UserDTO(user);
+    
+    // ??? ??? ??
+    return userDTO;
+};
+
+//{"result":{"email":"test@example.com","name":"??","preferCategory":["??","??","??"]}}
---
+++ src/index.js
@@ -0,0 +1,24 @@
+import cors from "cors";
+import dotenv from "dotenv";
+import express from "express";
+import { handleUserSignUp } from "./controllers/user.controller.js";
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT;
+
+app.use(cors()); // cors ?? ??
+app.use(express.static("public")); // ?? ?? ??
+app.use(express.json()); // request? ??? json?? ??? ? ??? ? (JSON ??? ?? body? ???? ??)
+app.use(express.urlencoded({ extended: false })); // ?? ?? ??? ??? ?? ??? ??
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.post("/api/v1/users/signup", handleUserSignUp);
+
+app.listen(port, () => {
+  console.log('Example app listening on port ${port}');
+});

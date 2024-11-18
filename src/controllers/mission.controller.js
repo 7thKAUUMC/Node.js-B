1181fadb3a4677151b6baa203a2ca3cb88af66e6
jih023 <jih023@naver.com>
2024-11-19 02:46:52 +0900

    8주차 미션 - swagger 문서 작성

--- src/controllers/mission.controller.js
+++ src/controllers/mission.controller.js
@@ -3,6 +3,72 @@ import { createMission, missionChallenge } from "../services/missioin.service.js
 import { bodyToMission, bodyToChallengeMission } from "../dtos/mission.dto.js";
 
 export const handleAddMission = async (req, res) => {
+    /*
+    #swagger.summary = '미션 추가하기 API';
+    #swagger.requestBody = {
+      required: true,
+      content: {
+        "application/json": {
+          schema: {
+            type: "object",
+            properties: {
+              storeId: { type: "number" },
+              regionId: { type: "number" },
+              reward: { type: "number" },
+              deadline: { type: "string", format: "date-time" }
+            }
+          }
+        }
+      }
+    };
+    #swagger.responses[200] = {
+      description: "미션 추가 성공 응답",
+      content: {
+        "application/json": {
+          schema: {
+            type: "object",
+            properties: {
+              resultType: { type: "string", example: "SUCCESS" },
+              error: { type: "object", nummable: true, example: null },
+              success: {
+                type: "object",
+                properties: {
+                  storeId: { type: "number" },
+                  regionId: { type: "number" },
+                  reward: { type: "number" },
+                  deadline: { type: "string", format: "date-time" },
+                  mission_spec: { type: "string" },
+                  createdAt: { type: "string", format: "date-time" }
+                }
+              }
+            }
+          }
+        }
+      }
+    };
+    #swagger.responses[400] = {
+      description: "미션 추가 실패 응답",
+      content: {
+        "application/json": {
+          schema: {
+            type: "object",
+            properties: {
+              resultType: { type: "string", example: "FAIL" },
+              error: {
+                type: "object",
+                properties: {
+                  errorCode: { type: "string", example: "M001" },
+                  reason: { type: "string" },
+                  data: { type: "object" }
+                }
+              },
+              success: { type: "object", nullable: true, example: null }
+            }
+          }
+        }
+      }
+    };
+    */
     console.log("request to add mission");
     console.log("body:", req.body);
 
@@ -11,6 +77,72 @@ export const handleAddMission = async (req, res) => {
 };
 
 export const handleChallengeMission = async (req, res) => {
+    /*
+    #swagger.summary = '진행 중인 미션에 추가하기 API';
+    #swagger.requestBody = {
+      required: true,
+      content: {
+        "application/json": {
+          schema: {
+            type: "object",
+            properties: {
+              missionId: { type: "number" },
+              userId: { type: "number" },
+              storeId: { type: "number" },
+              regionId: { type: "number" },
+              status: { type: "string" }
+            }
+          }
+        }
+      }
+    };
+    #swagger.responses[200] = {
+      description: "진행 중인 미션에 추가 성공 응답",
+      content: {
+        "application/json": {
+          schema: {
+            type: "object",
+            properties: {
+              resultType: { type: "string", example: "SUCCESS" },
+              error: { type: "object", nummable: true, example: null },
+              success: {
+                type: "object",
+                properties: {
+                  missionId: { type: "number" },
+                  userId: { type: "number" },
+                  storeId: { type: "number" },
+                  regionId: { type: "number" },
+                  status: { type: "string" }
+                }
+              }
+            }
+          }
+        }
+      }
+    };
+    #swagger.responses[400] = {
+      description: "진행 중인 미션에 추가 실패 응답",
+      content: {
+        "application/json": {
+          schema: {
+            type: "object",
+            properties: {
+              resultType: { type: "string", example: "FAIL" },
+              error: {
+                type: "object",
+                properties: {
+                  errorCode: { type: "string", example: "C001" },
+                  reason: { type: "string" },
+                  data: { type: "object" }
+                }
+              },
+              success: { type: "object", nullable: true, example: null }
+            }
+          }
+        }
+      }
+    };
+    */
     console.log("request to add challengemission");
     console.log("body:", req.body);
 
